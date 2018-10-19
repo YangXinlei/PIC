a372c033dda5e7eff2ad26f7a962bdedb9b73bfa
yangxinlei <yangxinlei@kuaishou.com>
2018-10-19 17:43:40 +0800

    support adjust alpha

--- PicViewer/MyWindow.m
+++ PicViewer/MyWindow.m
@@ -10,4 +10,17 @@
 
 @implementation MyWindow
 
+- (void)keyDown:(NSEvent *)event {
+    float interval = 0.15;
+    if (event.modifierFlags & NSEventModifierFlagOption) {
+        interval = 0.05;
+    }
+
+    if (event.keyCode == 126 || event.keyCode == 24) { // arrow up or +
+        self.alphaValue += interval;
+    } else if (event.keyCode == 125 || event.keyCode == 27) {
+        self.alphaValue = MAX(0.1, self.alphaValue - interval);
+    }
+}
+
 @end
